993f4896e6c4018fcf27b7538573ee60850d5025
Jimmy DC <jimmydalecleveland@gmail.com>
2022-09-18 11:35:57 -0600

    slightly more advanced zod

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ import { z } from "zod";
 // 	lightSaberColors: string[];
 // };
 const person = z.object({
-    name: z.string(),
-    age: z.number(),
-    lightSaberColors: z.array(z.string()),
+    name: z.string().transform((val) => val.toUpperCase()),
+    hair_color: z.string(),
 });
 async function fetchPerson() {
     const results = await fetch("https://swapi.dev/api/people/1/").then((res) => res.json());
+    console.log(results);
     const parsedResults = person.parse(results);
+    console.log(parsedResults);
     return parsedResults;
 }
 fetchPerson().then((person) => {
     console.log(person.name);
-    // console.log(person.age);
-    // console.log(person.lightSaberColors[0]);
+    console.log(person.hair_color);
 });
--- index.ts
+++ index.ts
@@ -8,23 +8,23 @@ import { z } from "zod";
 // };
 
 const person = z.object({
-  name: z.string(),
-  age: z.number(),
-  lightSaberColors: z.array(z.string()),
+  name: z.string().min(1).max(100),
+  hair_color: z.string().transform((val) => val.toUpperCase()),
 });
 
 type Person = z.infer<typeof person>;
 
 async function fetchPerson(): Promise<Person> {
-  const results = await fetch("https://swapi.dev/api/people/1/").then(
-    (res) => res.json() as Promise<Person>
+  const results = await fetch("https://swapi.dev/api/people/1/").then((res) =>
+    res.json()
   );
+  console.log("results before parse", results);
   const parsedResults = person.parse(results);
+  console.log("parsed results", parsedResults);
   return parsedResults;
 }
 
 fetchPerson().then((person) => {
   console.log(person.name);
-  // console.log(person.age);
-  // console.log(person.lightSaberColors[0]);
+  console.log(person.hair_color);
 });
