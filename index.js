3342240a991f978241d5a16c7753aea7cafacba6
Jimmy DC <jimmydalecleveland@gmail.com>
2022-09-18 10:55:23 -0600

    simple zod example
    throws an error at runtime for types missing

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import fetch from "node-fetch";
+import { z } from "zod";
+// type Person = {
+// 	name: string;
+// 	age: number;
+// 	lightSaberColors: string[];
+// };
+const person = z.object({
+    name: z.string(),
+    age: z.number(),
+    lightSaberColors: z.array(z.string()),
+});
 async function fetchPerson() {
     const results = await fetch("https://swapi.dev/api/people/1/").then((res) => res.json());
-    return results;
+    const parsedResults = person.parse(results);
+    return parsedResults;
 }
 fetchPerson().then((person) => {
     console.log(person.name);
-    console.log(person.age);
-    console.log(person.lightSaberColors[0]);
+    // console.log(person.age);
+    // console.log(person.lightSaberColors[0]);
 });
--- index.ts
+++ index.ts
@@ -1,20 +1,30 @@
 import fetch from "node-fetch";
+import { z } from "zod";
 
-type Person = {
-	name: string;
-	age: number;
-	lightSaberColors: string[];
-};
+// type Person = {
+// 	name: string;
+// 	age: number;
+// 	lightSaberColors: string[];
+// };
+
+const person = z.object({
+  name: z.string(),
+  age: z.number(),
+  lightSaberColors: z.array(z.string()),
+});
+
+type Person = z.infer<typeof person>;
 
 async function fetchPerson(): Promise<Person> {
-	const results = await fetch("https://swapi.dev/api/people/1/").then(
-		(res) => res.json() as Promise<Person>
-	);
-	return results;
+  const results = await fetch("https://swapi.dev/api/people/1/").then(
+    (res) => res.json() as Promise<Person>
+  );
+  const parsedResults = person.parse(results);
+  return parsedResults;
 }
 
 fetchPerson().then((person) => {
-	console.log(person.name);
-	console.log(person.age);
-	console.log(person.lightSaberColors[0]);
+  console.log(person.name);
+  // console.log(person.age);
+  // console.log(person.lightSaberColors[0]);
 });
